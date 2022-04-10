f9ad9aeec56325f1f2e569846c485cef3cce4efb
Noman08 <53017185+Noman08@users.noreply.github.com>
2022-04-10 18:16:28 +0200

    Update fetchApi.js

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
+      'x-rapidapi-key': 'd888fca5b9msh0a71f5cad583dfbp1bbf5ajsnad84b021aa7a' ,
     },
   });
     
